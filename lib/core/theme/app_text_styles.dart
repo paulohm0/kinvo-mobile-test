891c45e59e1f55f7d12ff6ad2cb05167c0629103
Paulo Henrique <paulohenrique1019@gmail.com>
2025-05-15 17:02:32 -0300

    add core/theme

---
+++ lib/core/theme/app_text_styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'app_text_sizes.dart';
+
+class AppTextStyles {
+  static const TextStyle body = TextStyle(
+    fontSize: AppTextSizes.md,
+    color: Colors.black,
+  );
+}
