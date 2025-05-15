3fb7bb75ee98c2e329496d36f6b5910d2aa6df09
Paulo Henrique <paulohenrique1019@gmail.com>
2025-05-15 17:03:11 -0300

    wip: homeview

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:kinvo_mobile_test/ui/home/view/home_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kinvo Mobile Test',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: HomeView(),
+    );
+  }
+}
