3ef55fb73fd016e1dec052e9c76a86d0344fda1e
Paulo Henrique <paulohenrique1019@gmail.com>
2025-05-16 11:50:13 -0300

    homeview finalized

--- lib/ui/home/view/home_view.dart
+++ lib/ui/home/view/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kinvo_mobile_test/shared/widgets/app_bar_custom.dart';
+import 'package:kinvo_mobile_test/ui/home/widgets/home_card.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -7,7 +8,31 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(appBar: CustomAppBar(title: 'Kinvo Mobile Test')),
+      child: Scaffold(
+        appBar: CustomAppBar(title: 'Flutter Mobile Test'),
+        body: Column(
+          children: [
+            HomeCard(
+              icon: Icons.add_chart,
+              title: 'Ações',
+              subtitle: 'Nacionais',
+              routeName: '/national_stocks',
+            ),
+            HomeCard(
+              icon: Icons.account_balance_wallet_outlined,
+              title: 'Fundos',
+              subtitle: 'De Investimentos',
+              routeName: '/investiments_funds',
+            ),
+            HomeCard(
+              icon: Icons.monetization_on_outlined,
+              title: 'Previdências',
+              subtitle: 'Privadas',
+              routeName: '/private_pension',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/ui/home/widgets/home_card.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:kinvo_mobile_test/core/theme/app_colors.dart';
+import 'package:kinvo_mobile_test/core/theme/app_text_styles.dart';
+
+class HomeCard extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String subtitle;
+  final String routeName;
+  const HomeCard({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.subtitle,
+    required this.routeName,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: AppColors.white,
+      elevation: 1,
+      margin: const EdgeInsets.only(top: 24.0, left: 24.0, right: 24.0),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(12),
+        onTap: () {
+          Navigator.pushNamed(context, routeName);
+        },
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 36),
+          child: Row(
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    Container(
+                      height: 60,
+                      width: 60,
+                      decoration: BoxDecoration(
+                        color: AppColors.mainBackground,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Icon(icon, size: 36, color: AppColors.primary),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(title, style: AppTextStyles.titleCard),
+                          Text(subtitle, style: AppTextStyles.subTitleCard),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
