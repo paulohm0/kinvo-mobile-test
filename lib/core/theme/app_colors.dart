787718020dd65f3297959f798b1613ee2622b54e
paulohm0 <paulohenrique1019@gmail.com>
2025-05-18 13:04:06 -0300

    refactor theme

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -4,6 +4,7 @@ class AppColors {
   static const Color red = Color(0xFFE85D1F);
   static const Color blue = Color(0xFF36C4D6);
   static const Color grey = Color(0xFF627179);
+  static const Color grey2 = Color(0xFF4D595E);
   static const Color green = Color(0xFFAED335);
   static const Color white = Color(0xFFFFFFFF);
   static const Color yellow = Color(0xFFF8C22E);
--- lib/core/theme/app_text_styles.dart
+++ lib/core/theme/app_text_styles.dart
@@ -9,14 +9,32 @@ class AppTextStyles {
     fontWeight: FontWeight.bold,
     color: AppColors.primary,
   );
-  static const TextStyle titleCard = TextStyle(
+  static const TextStyle titleHomeCard = TextStyle(
     color: AppColors.primary,
     fontSize: AppTextSizes.titleCard,
     fontWeight: FontWeight.w700,
   );
-  static const TextStyle subTitleCard = TextStyle(
+  static const TextStyle subTitleHomeCard = TextStyle(
     color: AppColors.grey,
     fontSize: AppTextSizes.small,
     fontWeight: FontWeight.w600,
+  );
+  static const TextStyle titleStockCard = TextStyle(
+    color: AppColors.grey2,
+    fontSize: AppTextSizes.titleCard,
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle subTitleStockCard = TextStyle(
+    color: AppColors.grey,
+    fontSize: AppTextSizes.medium,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle minValueStockCard = TextStyle(
+    color: AppColors.grey2,
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle profitValueStockCard = TextStyle(
+    color: AppColors.red,
+    fontWeight: FontWeight.w700,
   );
 }
