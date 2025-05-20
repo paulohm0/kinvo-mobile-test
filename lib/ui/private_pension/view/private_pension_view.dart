5d8e0dd818edbd16420856d6229e9577c116ad62
Paulo Henrique <paulohenrique1019@gmail.com>
2025-05-20 12:10:33 -0300

    wip: private pension view

--- lib/ui/private_pension/view/private_pension_view.dart
+++ lib/ui/private_pension/view/private_pension_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:kinvo_mobile_test/shared/widgets/app_bar_custom.dart';
+import 'package:kinvo_mobile_test/shared/widgets/loading_error_wrapper.dart';
+import 'package:kinvo_mobile_test/ui/private_pension/view_model/private_pensions_view_model.dart';
+import 'package:provider/provider.dart';
 
 class PrivatePensionView extends StatelessWidget {
   const PrivatePensionView({super.key});
@@ -9,6 +12,24 @@ class PrivatePensionView extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         appBar: CustomAppBar(title: 'Previdências', showBackButton: true),
+        body: Consumer<PrivatePensionsViewModel>(
+          builder: (context, viewModel, _) {
+            return LoadingErrorWrapper(
+              isLoading: viewModel.isLoading,
+              error: viewModel.error,
+              child: ListView.builder(
+                itemCount: viewModel.pensions.length,
+                itemBuilder: (context, index) {
+                  final pension = viewModel.pensions[index];
+                  return ListTile(
+                    title: Text(pension.name),
+                    subtitle: Text(pension.type),
+                  );
+                },
+              ),
+            );
+          },
+        ),
       ),
     );
   }
